2df52fe68a3be51e05dc5fa33b6495895e03039c
ben <benjamin.j.cooper@student.uts.edu.au>
2025-03-23 18:59:36 +1100

    Send and Recive points, launch file new works

--- src/selfie_cpp_pkg/src/path_planning_node.cpp
+++ src/selfie_cpp_pkg/src/path_planning_node.cpp
@@ -1,37 +1,39 @@
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/point.hpp"
 
 class PathPlanningNode: public rclcpp::Node
 {
     public:
         PathPlanningNode(): Node("path_planning")
         {
-            subscription_ = this->create_subscription<example_interfaces::msg::String>("raw_points",10,std::bind(&Smartphone::callbackRawGoals,this,std::placeholders::_1));
+            subscription_ = this->create_subscription<geometry_msgs::msg::Point>("raw_points",10,std::bind(&PathPlanningNode::callbackRawGoals,this,std::placeholders::_1));
             RCLCPP_INFO(this->get_logger(),"path_planning has been started");
 
             //subscribe to topics
         }
 
     private:
-        void callbackRawGoals(const example_interfaces::msg::String::SharedPtr msg) //use cosnt for all callbacks
+        void callbackRawGoals(const geometry_msgs::msg::Point::SharedPtr msg) //use cosnt for all callbacks
         {
-            RCLCPP_INFO(this->get_logger(),"I heard: '%s'",msg->data.c_str());
-            if(msg->data == 99999)
-            {
-                isSameSegemnt = false
-            }
+            RCLCPP_INFO(this->get_logger(), "Point received: x=%.2f, y=%.2f, z=%.2f", msg->x, msg->y, msg->z);
+
+            // if(msg->data == 99999)
+            // {
+            //     isSameSegemnt = false
+            // }
         }
 
-        rclcpp::Subscription<example_interfaces::msg::String>::SharedPtr subscription_;
+        rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr subscription_;
 
-        bool isSameSegemnt = true;
+        //bool isSameSegemnt = true;
         //vector or vector of points
 
-}
+};
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc,argv);
-    auto node = std::make_sahred<PathPlanningNode>();
+    auto node = std::make_shared<PathPlanningNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
--- src/selfie_cpp_pkg/src/raw_goal_node.cpp
+++ src/selfie_cpp_pkg/src/raw_goal_node.cpp
@@ -1,55 +1,71 @@
 #include "rclcpp/rclcpp.hpp"
-
-struct point()
-{
-    double x,y;
-};
+#include "geometry_msgs/msg/point.hpp"
+#include "std_srvs/srv/trigger.hpp"  
 
 class RawGoalNode: public rclcpp::Node
 {
     public:
     RawGoalNode(): Node("raw_goals")
         {
-            publisher_ = this->create_publisher<example_interfaces::msg::String>("raw_points",10);
-            timer_ = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&RawGoalNode::publishNews,this));
-            RCLCPP_INFO(this->get_logger(), "raw goals node running")
+            publisher_ = this->create_publisher<geometry_msgs::msg::Point>("raw_points",10);
+            service_ = this->create_service<std_srvs::srv::Trigger>("send_raw_goals", std::bind(&RawGoalNode::callbackSendRawGoals, this, std::placeholders::_1,std::placeholders::_2));
+            //timer_ = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&RawGoalNode::publishRawPoint,this));
+            RCLCPP_INFO(this->get_logger(), "raw goals node running");
         }
+
 
     private:
-        void publishRawPoint()
+        // void publishRawPoint()
+        // {
+        //     if(publishPoints)
+        //     {
+        //         auto msg = geometry_msgs::msg::Point();
+        //         msg.x = 1;
+        //         msg.y = 2;
+        //         msg.z = 3;
+        //         publisher_->publish(msg);
+        //     }
+        // }
+
+        void callbackSendRawGoals(std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+            std::shared_ptr<std_srvs::srv::Trigger::Response> response)
         {
-            auto msg = example_interfaces::msg::String();
-            msg.data = std::string("Hi, this is raw points");
-            publisher_->publish(msg);
+            (void)request;
+            //publishPoints = true;
+            publishRawPointsManual();
+            response->message = "Ben is awesome";
+            response->success = true;
         }
 
-        rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr publisher_;
-        rclcpp::TimerBase::SharedPtr timer_;
-            
-        point p1,p2,p3,p4,p5;
-        p1.x = 1;
-        p1.y = 2;
+        rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
+        rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr service_; 
+        //rclcpp::TimerBase::SharedPtr timer_;
 
-        p2.x = 1;
-        p2.y = 2;
+        bool publishPoints = false;
+        unsigned int numPoints = 5;
 
-        p3.x = 1;
-        p3.y = 2;
+        //Hard Coded Values
+        int pointDataX[5] = {1,2,3,4,5};
+        int pointDataY[5] = {1,1,2,3,0};
 
-        p4.x = 1;
-        p4.y = 2;
-
-        p5.x = 1;
-        p5.y = 2;
-
-        point HardCodedGoals = [p1,p2,p3,p4,p5];
-
-}
+        void publishRawPointsManual()
+        {
+            for(unsigned int i = 0; i != numPoints; i++)
+            {
+                auto msg = geometry_msgs::msg::Point();
+                msg.x = pointDataX[i];
+                msg.y = pointDataY[i];
+                msg.z = 0;
+                publisher_->publish(msg);
+            }
+        }
+    
+};
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc,argv);
-    auto node = std::make_sahred<RawGoalNode>();
+    auto node = std::make_shared<RawGoalNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
